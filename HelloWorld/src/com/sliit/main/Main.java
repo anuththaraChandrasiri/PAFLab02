90680f02f2cbce813128636554983960aa9d18d3
anuththaraChandrasiri <sakchandrasiri@gmail.com>
2021-02-17 18:51:04 +0530

    First commit

---
+++ HelloWorld/src/com/sliit/main/Main.java
@@ -0,0 +1,11 @@
+package com.sliit.main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Adding a new project to the GitHub repository using Eclipse IDE");
+
+	}
+
+}
